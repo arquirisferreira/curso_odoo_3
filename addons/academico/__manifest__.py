335e91c64e183f8def6b7c64db74ff44ac1ee0d6
arquirisferreira <arquiris@myia.com.br>
2020-11-10 21:44:08 +0000

    Reports

--- addons/academico/__manifest__.py
+++ addons/academico/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'board'],
 
     # always loaded
     'data': [
@@ -29,6 +29,8 @@
         'views/templates.xml',
         'views/academico.xml',
         'views/partner.xml',
+        'report.xml',
+        'views/session_board.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
